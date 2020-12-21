a816516906f1ad2adc03c69551b076e1232d33ae
Vladimir-Rudnik <cardworker.fox@gmail.com>
2020-12-21 21:44:14 +0300

    Изменение во второй задаче.

--- homeworks/task2.py
+++ homeworks/task2.py
@@ -1,31 +1,5 @@
-time = input("Введите время в секундах: ")
-ss = int(time)
-hh = ss // 3600
-if ss < 3600:
-    mm = ss // 60
-    ss2 = ss - mm * 60
-else:
-    mm = ss // 60 - 60
-    ss2 = ss - (mm + 60) * 60
-
-if hh < 10:
-    hh1 = "0"
-    hh = str(hh)
-    hh = hh1 + hh
-else:
-    hh = str(hh)
-if mm < 10:
-    mm1 = "0"
-    mm = str(mm)
-    mm = mm1 + mm
-else:
-    mm = str(mm)
-if ss2 < 10:
-    ss1 = "0"
-    ss2 = str(ss2)
-    ss2 = ss1 + ss2
-else:
-    ss2 = str(ss2)
-
-time2 = (hh, mm, ss2)
-print(f"Получаем: {':'.join(time2)}")
+time = int(input("Введите время в секундах: "))
+hours = time // 3600
+minutes = (time // 60) - (hours * 60)
+seconds = time % 60
+print (f"{hours:02}:{minutes:02}:{seconds:02}")
